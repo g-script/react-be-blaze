189ce9ba4eda8b4938735fbbdf4b27d62db406f6
Nicolas Goudry <goudry.nicolas@gmail.com>
2016-12-18 15:05:56 +0100

    Add lib entry point

---
+++ src/index.js
@@ -0,0 +1,109 @@
+// Objects
+import Container from './objects/Container'
+import Drawer from './objects/Drawer'
+import { Cell, Grid } from './objects/Grid'
+import Image from './objects/Image'
+import { MediaBody, MediaImage } from './objects/Media'
+import Modal from './objects/Modal'
+import { Panel, PanelContainer } from './objects/Panel'
+
+// Components
+import { Accordion, AccordionItem } from './components/Accordion'
+import Address from './components/Address'
+import Alert from './components/Alert'
+import Avatar from './components/Avatar'
+import Badge from './components/Badge'
+import Breadcrumb from './components/Breadcrumb'
+import Bubble from './components/Bubble'
+import { Button, ButtonGroup } from './components/Button'
+import { Card, CardBody, CardDivider, CardFooter, CardHeader, CardItem } from './components/Card'
+import Heading from './components/Heading'
+import { Choice, Fieldset, Input, InputGroup, Select, TextArea } from './components/Input'
+import { List, ListItem } from './components/List'
+import { Nav, NavItem } from './components/Nav'
+import Overlay from './components/Overlay'
+import { Progress, ProgressBar } from './components/Progress'
+import Range from './components/Range'
+import { Table, TableBody, TableCell, TableHead, TableRow } from './components/Table'
+import { Tab, Tabs, TabHeading } from './components/Tab'
+import Tag from './components/Tag'
+import Toast from './components/Toast'
+import Toggle from './components/Toggle'
+import { Tree, TreeItem } from './components/Tree'
+import { Abbr, Blockquote, Code, Highlight, Kbd, Link, Loud, Mono, Paragraph, Quiet } from './components/Typography'
+
+// Utilities
+import Align from './utilities/Align'
+
+// Named exports
+export {
+  // Objects
+  Container,
+  Drawer,
+  Cell,
+  Grid,
+  Image,
+  MediaBody,
+  MediaImage,
+  Modal,
+  Panel,
+  PanelContainer,
+  // Components
+  Accordion,
+  AccordionItem,
+  Address,
+  Alert,
+  Avatar,
+  Badge,
+  Breadcrumb,
+  Bubble,
+  Button,
+  ButtonGroup,
+  Card,
+  CardBody,
+  CardDivider,
+  CardFooter,
+  CardHeader,
+  CardItem,
+  Heading,
+  Choice,
+  Fieldset,
+  Input,
+  InputGroup,
+  Select,
+  TextArea,
+  List,
+  ListItem,
+  Nav,
+  NavItem,
+  Overlay,
+  Progress,
+  ProgressBar,
+  Range,
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableRow,
+  Tab,
+  Tabs,
+  TabHeading,
+  Tag,
+  Toast,
+  Toggle,
+  Tree,
+  TreeItem,
+  // Typography
+  Abbr,
+  Blockquote,
+  Code,
+  Highlight,
+  Kbd,
+  Link,
+  Loud,
+  Mono,
+  Paragraph,
+  Quiet,
+  // Utilities
+  Align
+}
