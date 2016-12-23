6869584cb5fd3117af308f82f3b0c2ca6f290043
Nicolas Goudry <goudry.nicolas@gmail.com>
2016-12-23 19:44:06 +0100

    Add Cells doc

--- src/app/Objects.js
+++ src/app/Objects.js
@@ -3,6 +3,8 @@ import { Alert, Cell, Heading, Link, Paragraph } from 'react-be-blaze'
 
 import Containers from './showcase/objects/Containers'
 import Grids from './showcase/objects/Grids'
+import Cells from './showcase/objects/Cells'
+
 const Objects = () => (
   <Cell>
     <Heading size='super'>Objects</Heading>
@@ -12,6 +14,7 @@ const Objects = () => (
     </Paragraph>
     <Containers />
     <Grids />
+    <Cells />
   </Cell>
 )
 
---
+++ src/app/showcase/objects/Cells.js
@@ -0,0 +1,88 @@
+import React from 'react'
+import { Code, Heading, Link, Table, TableBody, TableCell, TableHead, TableRow } from 'react-be-blaze'
+
+const Cells = () => (
+  <div>
+    <Heading size='xlarge'>Cells</Heading>
+    <Code multiline>
+      {
+`import React from 'react'
+import { Cell } from 'react-be-blaze'
+
+const Example = () => (
+  <Cell align={…}
+        fixed={…}
+        hidden={…}
+        noGutter={…}
+        offset={…}
+        responsiveness={…}
+        size={…}
+        width={…}>
+    Example
+  </Cell>)
+
+export default Example`
+      }
+      </Code>
+    <Table striped>
+      <TableHead>
+        <TableCell className='props-table__small-column'>Props</TableCell>
+        <TableCell>Values</TableCell>
+        <TableCell className='props-table__small-column'>Default</TableCell>
+        <TableCell>Description</TableCell>
+      </TableHead>
+      <TableBody>
+        <TableRow>
+          <TableCell className='props-table__small-column'>align</TableCell>
+          <TableCell>top, center, bottom</TableCell>
+          <TableCell className='props-table__small-column'>—</TableCell>
+          <TableCell>Vertical alignment.</TableCell>
+        </TableRow>
+        <TableRow>
+          <TableCell className='props-table__small-column'>fixed</TableCell>
+          <TableCell>true, false</TableCell>
+          <TableCell className='props-table__small-column'>false</TableCell>
+          <TableCell>Allow fixed cell width.</TableCell>
+        </TableRow>
+        <TableRow>
+          <TableCell className='props-table__small-column'>hidden</TableCell>
+          <TableCell>true, false</TableCell>
+          <TableCell className='props-table__small-column'>false</TableCell>
+          <TableCell>Hide cell.</TableCell>
+        </TableRow>
+        <TableRow>
+          <TableCell className='props-table__small-column'>noGutter</TableCell>
+          <TableCell>true, false</TableCell>
+          <TableCell className='props-table__small-column'>false</TableCell>
+          <TableCell>Remove cell gutters.</TableCell>
+        </TableRow>
+        <TableRow>
+          <TableCell className='props-table__small-column'>offset</TableCell>
+          <TableCell>Any number between 5 and 100 (incremented by 5)</TableCell>
+          <TableCell className='props-table__small-column'>—</TableCell>
+          <TableCell>Cell horizontal offset in percent.</TableCell>
+        </TableRow>
+        <TableRow>
+          <TableCell className='props-table__small-column'>responsiveness</TableCell>
+          <TableCell>{`arrayOf({ size: small|medium|large, width: number })`}</TableCell>
+          <TableCell className='props-table__small-column'>—</TableCell>
+          <TableCell>Responsive suffix (see <Link href='http://blazecss.com/objects/grid/#responsive-suffixes' target='_blank'>BlazeCSS doc</Link>).</TableCell>
+        </TableRow>
+        <TableRow>
+          <TableCell className='props-table__small-column'>size</TableCell>
+          <TableCell>xsmall, small, medium, large, xlarge, super</TableCell>
+          <TableCell className='props-table__small-column'>—</TableCell>
+          <TableCell>Screen size for cell width to be applied (width must be provided too).</TableCell>
+        </TableRow>
+        <TableRow>
+          <TableCell className='props-table__small-column'>width</TableCell>
+          <TableCell>Any number between 5 and 100 (incremented by 5)</TableCell>
+          <TableCell className='props-table__small-column'>—</TableCell>
+          <TableCell>Cell width in percent.</TableCell>
+        </TableRow>
+      </TableBody>
+    </Table>
+  </div>
+)
+
+export default Cells
