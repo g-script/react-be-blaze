4ea68a587c896978759fb8a6bed4ff12a8ee06ac
Nicolas Goudry <goudry.nicolas@gmail.com>
2016-12-23 15:48:03 +0100

    Add Objects and Components route components

--- src/app/Components.js
+++ src/app/Components.js
@@ -1,9 +1,13 @@
 import React from 'react'
-import { Cell, Heading, Link, Loud, Paragraph } from 'react-be-blaze'
+import { Alert, Cell, Heading, Link, Paragraph } from 'react-be-blaze'
 
 const Components = () => (
   <Cell>
-    <Heading size='xlarge'>Components</Heading>
+    <Heading size='super'>Components</Heading>
+    <Alert state='warning'>Please be patient, I’m currently writing these docs!</Alert>
+    <Paragraph>
+      Can’t wait? <Link href='https://github.com/g-script/react-be-blaze/tree/master/src/components' target='_blank'>Read the source code!</Link>
+    </Paragraph>
   </Cell>
 )
 
--- src/app/Objects.js
+++ src/app/Objects.js
@@ -1,9 +1,13 @@
 import React from 'react'
-import { Cell, Heading, Link, Loud, Paragraph } from 'react-be-blaze'
+import { Alert, Cell, Heading, Link, Paragraph } from 'react-be-blaze'
 
 const Objects = () => (
   <Cell>
-    <Heading size='xlarge'>Objects</Heading>
+    <Heading size='super'>Objects</Heading>
+    <Alert state='warning'>Please be patient, I’m currently writing these docs!</Alert>
+    <Paragraph>
+      Can’t wait? <Link href='https://github.com/g-script/react-be-blaze/tree/master/src/objects' target='_blank'>Read the source code!</Link>
+    </Paragraph>
   </Cell>
 )
 
