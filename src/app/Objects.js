1b655809c63276267095ccb30f3a69a67aae807a
Nicolas Goudry <goudry.nicolas@gmail.com>
2016-12-23 19:43:58 +0100

    Add Grids doc

--- src/app/Objects.js
+++ src/app/Objects.js
@@ -2,6 +2,7 @@ import React from 'react'
 import { Alert, Cell, Heading, Link, Paragraph } from 'react-be-blaze'
 
 import Containers from './showcase/objects/Containers'
+import Grids from './showcase/objects/Grids'
 const Objects = () => (
   <Cell>
     <Heading size='super'>Objects</Heading>
@@ -10,6 +11,7 @@ const Objects = () => (
       Can’t wait? <Link href='https://github.com/g-script/react-be-blaze/tree/master/src/objects' target='_blank'>Read the source code!</Link>
     </Paragraph>
     <Containers />
+    <Grids />
   </Cell>
 )
 
---
+++ src/app/showcase/objects/Grids.js
@@ -0,0 +1,64 @@
+import React from 'react'
+import { Code, Heading, Link, Paragraph, Table, TableBody, TableCell, TableHead, TableRow } from 'react-be-blaze'
+
+const Grids = () => (
+  <div>
+    <Heading size='xlarge'>Grids</Heading>
+    <Paragraph>
+      The grid is the easiest way to produce a fluid responsive layout. Its easy to use and very flexible.
+    </Paragraph>
+    <Code multiline>
+      {
+`import React from 'react'
+import { Grid } from 'react-be-blaze'
+
+const Example = () => (
+  <Grid align={…}
+        behavior={…}
+        noGutter={…}
+        size={…}
+        wrap={…}>
+    Example
+  </Grid>)
+
+export default Example`
+      }
+      </Code>
+    <Table striped>
+      <TableHead>
+        <TableCell className='props-table__small-column'>Props</TableCell>
+        <TableCell>Values</TableCell>
+        <TableCell className='props-table__small-column'>Default</TableCell>
+        <TableCell>Description</TableCell>
+      </TableHead>
+      <TableBody>
+        <TableRow>
+          <TableCell className='props-table__small-column'>align</TableCell>
+          <TableCell>top, center, bottom</TableCell>
+          <TableCell className='props-table__small-column'>—</TableCell>
+          <TableCell>Vertical alignment.</TableCell>
+        </TableRow>
+        <TableRow>
+          <TableCell className='props-table__small-column'>noGutter</TableCell>
+          <TableCell>true, false</TableCell>
+          <TableCell className='props-table__small-column'>false</TableCell>
+          <TableCell>Remove cells gutters.</TableCell>
+        </TableRow>
+        <TableRow>
+          <TableCell className='props-table__small-column'>responsiveness</TableCell>
+          <TableCell>{`arrayOf({ behavior: fit|full, size: small|medium|large })`}</TableCell>
+          <TableCell className='props-table__small-column'>—</TableCell>
+          <TableCell>Grid responsiveness (see <Link href='http://blazecss.com/objects/grid/#responsiveness' target='_blank'>BlazeCSS doc</Link>).</TableCell>
+        </TableRow>
+        <TableRow>
+          <TableCell className='props-table__small-column'>wrap</TableCell>
+          <TableCell>true, false</TableCell>
+          <TableCell className='props-table__small-column'>false</TableCell>
+          <TableCell>Wrap grid cells.</TableCell>
+        </TableRow>
+      </TableBody>
+    </Table>
+  </div>
+)
+
+export default Grids
