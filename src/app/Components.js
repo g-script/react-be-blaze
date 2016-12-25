0653fed368ef45e8c4abaa69a70b517344d6bbfe
Nicolas Goudry <goudry.nicolas@gmail.com>
2016-12-25 17:41:49 +0100

    Make alerts unclosable

--- src/app/Components.js
+++ src/app/Components.js
@@ -7,7 +7,7 @@ import Alerts from './components/Alerts'
 const Components = () => (
   <Cell>
     <Heading size='super'>Components</Heading>
-    <Alert state='warning'>Please be patient, I’m currently writing these docs!</Alert>
+    <Alert state='warning' closable={false}>Please be patient, I’m currently writing these docs!</Alert>
     <Paragraph>
       Can’t wait? <Link href='https://github.com/g-script/react-be-blaze/tree/master/src/components' target='_blank'>Read the source code!</Link>
     </Paragraph>
--- src/app/Objects.js
+++ src/app/Objects.js
@@ -8,7 +8,7 @@ import Cells from './objects/Cells'
 const Objects = () => (
   <Cell>
     <Heading size='super'>Objects</Heading>
-    <Alert state='warning'>Please be patient, I’m currently writing these docs!</Alert>
+    <Alert state='warning' closable={false}>Please be patient, I’m currently writing these docs!</Alert>
     <Paragraph>
       Can’t wait? <Link href='https://github.com/g-script/react-be-blaze/tree/master/src/objects' target='_blank'>Read the source code!</Link>
     </Paragraph>
