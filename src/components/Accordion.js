895737838a3458c91b0138a273edf226deec250f
Nicolas Goudry <goudry.nicolas@gmail.com>
2016-12-30 19:42:18 +0100

    Fixed #11

--- src/components/Accordion.js
+++ src/components/Accordion.js
@@ -3,6 +3,17 @@ import React, { PropTypes } from 'react'
 
 import { Card, CardItem } from './Card'
 
+const AccordionItem = () => (
+  <div>Missing Accordion container.</div>
+)
+
+AccordionItem.propTypes = {
+  children: PropTypes.any,
+  className: PropTypes.string,
+  id: PropTypes.string,
+  label: PropTypes.string
+}
+
 const Accordion = ({ children, className, ...rest }) => {
   className = classnames(
     className,
@@ -11,34 +22,27 @@ const Accordion = ({ children, className, ...rest }) => {
 
   return (
     <Card className={className} {...rest}>
-      {children}
+      {children.map((child) => {
+        let { children, className, id, label, ...rest } = child.props
+
+        className = classnames(
+          className,
+          'c-card__item'
+        )
+
+        return [
+          <input type='checkbox' id={id} />,
+          <label htmlFor={id} className='c-card__item'>{label}</label>,
+          <CardItem className={className} {...rest}>{children}</CardItem>
+        ]
+      })}
     </Card>
   )
 }
 
 Accordion.propTypes = {
-  children: PropTypes.any,
+  children: PropTypes.arrayOf(PropTypes.element),
   className: PropTypes.string
-}
-
-const AccordionItem = ({ children, className, id, label, ...rest }) => {
-  className = classnames(
-    className,
-    'c-card__item'
-  )
-
-  return [
-    <input type='checkbox' id={id} />,
-    <label htmlFor={id} className={className}>{label}</label>,
-    <CardItem className={className} {...rest}>{children}</CardItem>
-  ]
-}
-
-AccordionItem.propTypes = {
-  children: PropTypes.any,
-  className: PropTypes.string,
-  id: PropTypes.string,
-  label: PropTypes.string
 }
 
 export {
